50042a616bf74ad62241801ecea1eb2f13afa2c8
Stanislav <solarcoaster.st@gmail.com>
2024-02-15 13:01:19 +0200

    created figures, settings and useSmoothTransform hook for motion button

--- src/layout/layout.styles.js
+++ src/layout/layout.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledLayout = styled.div`
   background-color: #fff;
-  color: #000;
+  //color: #000;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
