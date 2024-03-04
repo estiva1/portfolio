555ef3946e56cfab6bf6b01600eb9b685df81952
Stanislav <solarcoaster.st@gmail.com>
2024-03-04 22:09:48 +0200

    added menu items

--- src/components/navigation/side-nav/side-nav.component.jsx
+++ src/components/navigation/side-nav/side-nav.component.jsx
@@ -29,31 +29,40 @@ const Curve = () => {
   );
 };
 
+const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Work",
+    href: "/work",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
+
 const SideNav = () => {
   return (
     <SideNavContainer variants={menuSlide} initial="initial" animate="enter" exit="exit">
-      {/* <div className={styles.body}>
-        <div
-          onMouseLeave={() => {
-            setSelectedIndicator(pathname);
-          }}
-          className={styles.nav}
-        >
-          <div className={styles.header}>
-            <p>Navigation</p>
-          </div>
-          {navItems.map((data, index) => {
-            return (
-              <Link
-                key={index}
-                data={{ ...data, index }}
-                isActive={selectedIndicator == data.href}
-                setSelectedIndicator={setSelectedIndicator}
-              ></Link>
-            );
-          })}
-        </div>
-      </div> */}
+      <div>
+        {/* <div className={styles.header}>
+          <p>Navigation</p>
+        </div> */}
+        {navItems.map((item, index) => {
+          return (
+            <p style={{ color: "white", fontSize: "5rem" }} key={index}>
+              {item.title}
+            </p>
+          );
+        })}
+      </div>
       <Curve />
     </SideNavContainer>
   );
